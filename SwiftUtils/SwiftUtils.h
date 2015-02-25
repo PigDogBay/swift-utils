6c2c0cf8c8b3e9b582444b77be9c35ed1e9ea97b
PigDogBay <pigdogbay@yahoo.co.uk>
2015-02-25 12:44:40 +0000

    Initial Commit

---
+++ SwiftUtils/SwiftUtils.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUtils.h
+//  SwiftUtils
+//
+//  Created by Mark Bailey on 25/02/2015.
+//  Copyright (c) 2015 MPD Bailey Technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftUtils.
+FOUNDATION_EXPORT double SwiftUtilsVersionNumber;
+
+//! Project version string for SwiftUtils.
+FOUNDATION_EXPORT const unsigned char SwiftUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUtils/PublicHeader.h>
+
+
