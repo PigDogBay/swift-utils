898369ad8a8f417aa3b5d24ebf8f2c6036bb7398
PigDogBay <pigdogbay@yahoo.co.uk>
2017-01-09 17:09:34 +0000

    Ported MissingLetters from Android

---
+++ SwiftUtils/MissingLetters.swift
@@ -0,0 +1,93 @@
+//
+//  MissingLetters.swift
+//  SwiftUtils
+//
+//  Created by Mark Bailey on 09/01/2017.
+//  Copyright © 2017 MPD Bailey Technology. All rights reserved.
+//
+
+import Foundation
+
+///  When doing anagram searches with blank letters, this class will help to highlight the letters in matching words that were
+///  used for the blank letters.
+open class MissingLetters
+{
+    let BUFFER_SIZE = 64
+    //No fixed sized arrays in Swift, closest is
+    //var buffer = [Int?](repeating: nil, count: 64)
+    fileprivate var buffer : [UInt32] = [0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+                                         0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0]
+    
+    fileprivate var positions : [Int] = [0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
+                                         0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0]
+    let lettersLen : Int
+    let letters : String
+    var builder  = ""
+    
+    /// Initialize class with anagram letters
+    ///
+    /// - Parameter letters: letters of the anagram not including blanks
+    public init(letters: String){
+        self.letters = letters
+        self.lettersLen = letters.unicodeScalars.count
+    }
+    
+    
+    /// Find the position of a missing letter in the word used in findPositions
+    ///
+    /// - Parameter index: index into array of letter positions
+    /// - Returns: letter position in the word
+    public func getPositionAt(index : Int) -> Int {
+        return positions[index]
+    }
+    
+    /// Finds a letter in the buffer
+    ///
+    /// - Parameter letter: letter to be found
+    /// - Returns: position in the buffer or -1 if not found
+    fileprivate func findLetter(_ letter : UInt32) -> Int {
+        //search upto length of the anagram letters
+        for i in 0..<lettersLen {
+            if buffer[i] == letter {
+                return i
+            }
+        }
+        return -1
+    }
+    
+    /// Missing letters = word letters - anagram letters
+    /// call getPositionAt() to access the positions of the letters in word
+    ///
+    /// - Parameter word: to find missing letters
+    /// - Returns: number of missing letters found
+    public func findPositions(word: String) -> Int {
+        var index = 0
+        //copy letters  into buffer
+        for c in letters.unicodeScalars {
+            buffer[index] = c.value
+            index = index + 1
+        }
+        
+        //use index as count for the positions found
+        index = 0
+        var indexInWord = 0;
+        for c in word.unicodeScalars {
+            //find letter in the buffer
+            let pos = findLetter(c.value)
+            if -1 == pos {
+                //letter not found, therefore must be a blank letter
+                positions[index] = indexInWord
+                index = index + 1
+            } else {
+                //remove char to stop it being found again
+                buffer[pos] = 0
+            }
+            indexInWord = indexInWord + 1
+            
+        }
+        
+        return index
+        
+    }
+    
+}
---
+++ SwiftUtilsTests/MissingLettersTests.swift
@@ -0,0 +1,98 @@
+//
+//  MissingLettersTests.swift
+//  SwiftUtils
+//
+//  Created by Mark Bailey on 09/01/2017.
+//  Copyright © 2017 MPD Bailey Technology. All rights reserved.
+//
+
+import XCTest
+import SwiftUtils
+
+class MissingLettersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testFindPosition1() {
+        let target = MissingLetters(letters: "gaim")
+        let actual = target.findPositions(word: "magic")
+        XCTAssertEqual(actual, 1)
+        XCTAssertEqual(target.getPositionAt(index: 0), 4)
+    }
+
+    func testFindPosition2() {
+        let target = MissingLetters(letters: "aage")
+        let actual = target.findPositions(word: "babbage")
+        XCTAssertEqual(actual, 3)
+        XCTAssertEqual(target.getPositionAt(index: 0), 0)
+        XCTAssertEqual(target.getPositionAt(index: 1), 2)
+        XCTAssertEqual(target.getPositionAt(index: 2), 3)
+    }
+    
+    func testFindPosition3() {
+        let target = MissingLetters(letters: "cigam")
+        let actual = target.findPositions(word: "magic")
+        XCTAssertEqual(actual, 0)
+    }
+    
+    func testFindPosition4() {
+        let target = MissingLetters(letters: "")
+        let actual = target.findPositions(word: "magic")
+        XCTAssertEqual(actual, 5)
+        XCTAssertEqual(target.getPositionAt(index: 0), 0)
+        XCTAssertEqual(target.getPositionAt(index: 2), 2)
+        XCTAssertEqual(target.getPositionAt(index: 4), 4)
+    }
+    
+    func testFindPosition5() {
+        let target = MissingLetters(letters: "magic")
+        let actual = target.findPositions(word: "")
+        XCTAssertEqual(actual, 0)
+    }
+
+    func testFindPosition6() {
+        let target = MissingLetters(letters: "magic")
+        let actual = target.findPositions(word: "cog")
+        XCTAssertEqual(actual, 1)
+        XCTAssertEqual(target.getPositionAt(index: 0), 1)
+    }
+    
+    func testFindPosition7() {
+        let target = MissingLetters(letters: "kayleigh")
+        let actual = target.findPositions(word: "breathtakingly")
+        XCTAssertEqual(actual, 6)
+        XCTAssertEqual(target.getPositionAt(index: 0), 0)
+        XCTAssertEqual(target.getPositionAt(index: 1), 1)
+        XCTAssertEqual(target.getPositionAt(index: 2), 4)
+        XCTAssertEqual(target.getPositionAt(index: 3), 6)
+        XCTAssertEqual(target.getPositionAt(index: 4), 7)
+        XCTAssertEqual(target.getPositionAt(index: 5), 10)
+    }
+    
+    func testFindPosition8() {
+        let target = MissingLetters(letters: "holly")
+        let actual = target.findPositions(word: "llanfairpwllgwyngyllgogeryqjxzchwyrndrobwllllantysiliogogogoch")
+        XCTAssertEqual(actual, 57)
+        XCTAssertEqual(target.getPositionAt(index: 0), 2)
+    }
+
+    func testPerformanceExample() {
+        let target = MissingLetters(letters: "wonderba")
+        self.measure {
+            for _ in 0 ..< 1000
+            {
+                // Put the code you want to measure the time of here.
+//                target.findPositions(word: "wonderful")
+            }
+        }
+    }
+    
+}
