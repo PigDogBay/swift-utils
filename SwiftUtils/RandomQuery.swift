548b0286560a17abde2a3a2024e79851fb6ae5c6
Mark Bailey <markpdbailey@gmail.com>
2020-08-14 11:45:21 +0100

    Added more random queries and tests

--- SwiftUtils/RandomQuery.swift
+++ SwiftUtils/RandomQuery.swift
@@ -12,11 +12,19 @@ public struct RandomQuery {
     let vowels = ["a","e","i","o","u"]
     let consonants = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
     let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    let examples = ["m?g??","moonstarer","z9","james-bond","scrabb++","kayleigh*","super@ted","..112332"]
     
     
     public init(){
         
     }
+    
+    public func anagram() -> String {
+        let v = Int.random(in: 1...5)
+        let c = Int.random(in: 2...10)
+        return anagram(numberOfVowels: v, numberOfConsonants: c)
+    }
+    
     public func anagram(numberOfVowels v: Int, numberOfConsonants c: Int) -> String {
         var builder = ""
         for _ in 1...v {
@@ -37,7 +45,25 @@ public struct RandomQuery {
             builder = builder + WordSearch.CROSSWORD_STR
         }
         return shuffle(ordered: builder)
+    }
+    
+    public func crossword() -> String {
+        let l = Int.random(in: 1...5)
+        let u = Int.random(in: 2...10)
+        return crossword(numberOfLetters: l, numberOfUnknowns: u)
+    }
 
+    
+    public func twoWords() -> String {
+        var anag = anagram() + " "
+        if anag.first == " " || anag.last == " " {
+            anag = shuffle(ordered: anag)
+        }
+        return shuffle(ordered: anag)
+    }
+    
+    public func example() -> String {
+        return examples.randomElement()!
     }
     
     private func shuffle(ordered : String) -> String {
--- SwiftUtilsTests/RandomQueryTests.swift
+++ SwiftUtilsTests/RandomQueryTests.swift
@@ -11,16 +11,56 @@ import XCTest
 import SwiftUtils
 
 class RandomQueryTests: XCTestCase {
+    let loop = 100
+    let wordSearch = WordSearch(wordList: WordList())
+    
+    private func check(query : String, expectedType : SearchType){
+        let preProcess = wordSearch.preProcessQuery(query)
+        XCTAssertEqual(query,preProcess)
+        let searchType = wordSearch.getQueryType(query)
+        XCTAssertEqual(searchType, expectedType)
+    }
 
     func testAnagram1(){
         let randomQuery = RandomQuery()
-        let actual = randomQuery.anagram(numberOfVowels: 3, numberOfConsonants: 6)
-        XCTAssertEqual(actual.length, 9)
+        for _ in 1...loop {
+            let actual = randomQuery.anagram(numberOfVowels: 3, numberOfConsonants: 6)
+            XCTAssertEqual(actual.length, 9)
+            check(query: actual, expectedType: .anagram)
+        }
+    }
+
+    func testAnagram2(){
+        let randomQuery = RandomQuery()
+        for _ in 1...loop {
+            let actual = randomQuery.anagram()
+            check(query: actual, expectedType: .anagram)
+        }
     }
 
     func testCrossword1(){
         let randomQuery = RandomQuery()
-        let actual = randomQuery.crossword(numberOfLetters: 3, numberOfUnknowns: 6)
-        XCTAssertEqual(actual.length, 9)
+        for _ in 1...loop {
+            let actual = randomQuery.crossword(numberOfLetters: 3, numberOfUnknowns: 6)
+            XCTAssertEqual(actual.length, 9)
+            check(query: actual, expectedType: .crossword)
+        }
     }
+    
+    func testCrossword2(){
+        let randomQuery = RandomQuery()
+        for _ in 1...loop {
+            let actual = randomQuery.crossword()
+            check(query: actual, expectedType: .crossword)
+        }
+    }
+
+    func testTwoWords1(){
+        let randomQuery = RandomQuery()
+        for _ in 1...loop {
+            let actual = randomQuery.twoWords()
+            check(query: actual, expectedType: .twoWordAnagram)
+        }
+    }
+
 }
